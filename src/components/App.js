d01d165e7001d46a6e56a5ed6c70c10aadf7e69f
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-11-04 19:52:53 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="root">
-      <p>"I am learning React. My life is getting better. "</p>
+      <p>"I am learning React. My life is getting better."</p>
     </div>
   );
 };
